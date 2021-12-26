cc7d190e6fb442bbcf9a907f591bf73cff02cd1e
Austin Baik <baik.austin@gmail.com>
2021-12-25 20:21:00 -0800

    commit tree 78d401e08d20c3dcbf45784d9dfec4619ed9204e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+
+
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+ }
+
+ function setBestCustomer() {
+     return bestCustomer = 'not bob';
+ }
+ 
+ 
+ function overwriteBestCustomer(name){
+     return bestCustomer = name; 
+ }
+ 
+ 
+const leastFavoriteCustomer = 'jon';
+ 
+function changeLeastFavoriteCustomer(name1) {
+     return leastFavoriteCustomer = name1;
+ }
+
